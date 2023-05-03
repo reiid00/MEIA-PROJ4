60d21c7f4571c66ad4901168cb6990c0973cef67
vasco reid <vascoreid@gmail.com>
2023-05-04 00:01:14 +0100

    Initial ROS2 PX4 packages - msgs, ros_com

---
+++ ROS2-PX4-Workspace/workspace/build/px4_msgs/px4_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vascor/MEIA-PROJ4/build/px4_msgs/px4_msgs__py
+# Build directory: /home/vascor/MEIA-PROJ4/build/px4_msgs/px4_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ROS2-PX4-Workspace/workspace/build/px4_ros_com/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/vascor/MEIA-PROJ4/install/px4_msgs/share/px4_msgs/package.sh"
