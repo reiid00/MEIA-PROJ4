c30818747cb473af88ad46e94efbecb6768e9b5b
vascoreid <vascoreid@gmail.com>
2023-06-19 22:18:25 +0100

    Fixed conflict in single_drone_liftoff

--- ROS2-PX4-Workspace/src/px4_ros_mas_com/px4_ros_mas_com/single_drone_liftoff.py
+++ ROS2-PX4-Workspace/src/px4_ros_mas_com/px4_ros_mas_com/single_drone_liftoff.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import rclpy
 from rclpy.node import Node
 from rclpy.clock import Clock
@@ -73,80 +73,4 @@ def main(args=None):
 
 
 if __name__ == '__main__':
-=======
-import rclpy
-from rclpy.node import Node
-from rclpy.clock import Clock
-from rclpy.qos import QoSProfile, HistoryPolicy, ReliabilityPolicy, DurabilityPolicy
-
-from px4_msgs.msg import OffboardControlMode
-from px4_msgs.msg import TrajectorySetpoint
-from px4_msgs.msg import VehicleCommand
-from px4_msgs.msg import VehicleControlMode
-from px4_msgs.msg import VehicleLocalPosition
-from px4_msgs.msg import VehicleAttitude
-
-from .droneControl import DroneControl
-from .droneListener import DroneListener
-
-from std_msgs.msg import String 
-
-import time
-import threading
-
-
-def run_drones(drones, drones_listeners):
-    while rclpy.ok():
-        for drone in drones:
-            drone_listener = next((drone_listener for drone_listener in drones_listeners if drone_listener.drone_num == drone.drone_num), None)
-            drone.update_current_position(drone_listener.current_position)
-            rclpy.spin_once(drone)
-            if drone.disarmmed : drones.remove(drone)
-        for drone in drones_listeners:
-            rclpy.spin_once(drone)
-
-def get_available_drones(drones_listeners, drones_running):
-    available_drones = []
-    for drone_listener in drones_listeners:
-        drone = next((drone_running for drone_running in drones_running if drone_running.drone_num == drone_listener.drone_num), None)
-        if drone == None : available_drones.append(drone_listener)
-    return available_drones
-
-
-
-def listen_drones_once(drones_listeners):
-    for drone in drones_listeners:
-        rclpy.spin_once(drone)
-
-def main(args=None):
-    rclpy.init(args=args)
-    drones_pos_listener = []
-    drone_pos_listener = DroneListener(2)
-    drone_pos_listener2 = DroneListener(1)
-    drones_pos_listener.append(drone_pos_listener)
-    drones_pos_listener.append(drone_pos_listener2)
-
-    listen_drones_once(drones_pos_listener)
-    drone_controls = []
-    drone_1__coordinates = []
-    offboard_control = DroneControl(2,[5.0,5.0,-10.0], drone_pos_listener2.current_position)
-    offboard_control2 = DroneControl(1,[50.0,90.0,-20.0], drone_pos_listener.current_position)
-    drone_controls.append(offboard_control)
-    drone_controls.append(offboard_control2)
-
-    drones_running = threading.Thread(target= run_drones, args=(drone_controls,drones_pos_listener,))
-    drones_running.start()  
-
-
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    #offboard_control.destroy_node()
-    #offboard_control2.destroy_node()
-    #rclpy.shutdown()
-
-
-if __name__ == '__main__':
->>>>>>> 8479205fe57e9e4d93b969944db802a935c5e598
     main()
