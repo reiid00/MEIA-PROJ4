9b52102e506a94f5ecc036c3b2c95fa287723c95
AgMeister7 <filipe.maia.prata@gmail.com>
2023-06-23 16:21:03 +0100

    lul, fixed wrong folders from build mistake

--- build/px4_msgs/px4_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/agmeister/proj4/MEIA-PROJ4/build/px4_msgs/px4_msgs__py
-# Build directory: /home/agmeister/proj4/MEIA-PROJ4/build/px4_msgs/px4_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/px4_ros_mas_com/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/agmeister/proj4/MEIA-PROJ4/install/px4_ros_mas_com'
